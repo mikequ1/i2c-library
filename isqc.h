32e627719fba7bd0c1e37a5d5fca76e91fea9afb
BigWillyMan420 <73449844+mikequ1@users.noreply.github.com>
2021-03-12 22:49:12 -0800

    some minor comments, setting up for update this weekend after i finish finals

--- isqc.h
+++ isqc.h
@@ -4,6 +4,9 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+//user initialization
+int SLAVE_ADDRESS;
+
 void sendDirection(byte var) {
     Wire.beginTransmission(SLAVE_ADDRESS);
     Wire.write(var);
@@ -11,15 +14,16 @@ void sendDirection(byte var) {
     mr_dir = var;
 }
 
+//supports every primitive data type
 template <typename T>
 void isqc_read(T& value)
 {
     Wire.requestFrom(SLAVE_ADDRESS, sizeof value);
-    byte* p = (byte*)&value;
+    byte* p = (byte*)&value; //changing value into an array of bytes
     for (int i = 0; i < sizeof value; i++) {
         *p++ = Wire.read();
     }
-    Serial.println(value);
+    Serial.println(value); //prints value, will delete later
 }
 
 template <typename T>
